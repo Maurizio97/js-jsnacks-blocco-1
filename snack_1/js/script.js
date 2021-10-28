60187f8d3207abd1d328d10d078d3dff06c69047
Maurizio <proiettomaurizio97@gmail.com>
2021-10-28 15:14:18 +0200

    snack 1 complete

--- snack_1/js/script.js
+++ snack_1/js/script.js
@@ -1,1 +1,24 @@
-console.log("ciao");
+/* 1 - L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+
+
+// creo una variabile per il div dove stamperò il numero
+const stamp = document.querySelector(".stamp");
+// creo il primo promt (x)
+const numX = parseInt(prompt("inserisci un numero"));
+// creo il secondo promt (y)
+const numY = parseInt(prompt("inserisci un altro  numero"));
+// confronto i due numeri
+    // se sono uguale gli dico che sono pari
+    if( numX === numY) {
+        stamp.innerHTML = `non c'è un numero maggiore`;
+    } else if (numX > numY ) {
+    // se è maggiore il numero "x" stampo "x"
+        stamp.innerHTML = `il maggione è: ${numX}`;
+    } else {
+        stamp.innerHTML = `il maggione è: ${numY}`;
+
+    }
+
+    // altrimenti stampo "y"
