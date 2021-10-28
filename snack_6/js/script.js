a3850c535d42507e9c3fb58d00f9ed536b8e965f
Maurizio <proiettomaurizio97@gmail.com>
2021-10-28 17:37:45 +0200

    es 6 complete

--- snack_6/js/script.js
+++ snack_6/js/script.js
@@ -4,19 +4,26 @@
 // creo una variabile per il div dove stamperò la somma dei numeri
 const stamp = document.querySelector(".stamp");
 // creo un array vuoto
-const randNum = [];
+let randNum = [];
+
+
 // creo un ciclo nella quale creerò una serie di numeri random
-while (randNum.length < 10) {
+while (randNum.length < 50) {
+    //creo una variabile che diventa vera ogni volta che un numero è diverso da quelli della lista
+    let diffNumber = false;
     // genero un numero random 
-    const numGen = Math.floor(Math.random() * 10 + 1);
-    randNum.push(numGen);
+    let numGen = Math.floor(Math.random() * 100 + 1);
 
     //confronto il numero generato con tutti quelli della lista partendo dal primo
     for(let i = 0; i < randNum.length; i++) {
-        console.log(randNum);
-        // se il numero è diverso da tutti quelli della lista lo inserisco 
-        //if ( numGen != randNum[i]){
-       // }
+        // se il numero è uguale a uno di quelli della lista la variabile di controllo diventa vera
+        if ( randNum[i] == numGen){
+            diffNumber = true
+        }
+    }
+        
+    if (!diffNumber){
+        randNum.push(numGen);
     }
 }
-    //console.log(randNum);
+console.log(randNum);
