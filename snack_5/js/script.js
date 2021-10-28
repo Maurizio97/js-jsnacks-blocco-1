312f414b6b5450ea5fa00bbb4f84dca5626226c4
Maurizio <proiettomaurizio97@gmail.com>
2021-10-28 16:42:51 +0200

    es 5 complete

--- snack_5/js/script.js
+++ snack_5/js/script.js
@@ -1,1 +1,17 @@
-console.log("ciao");
+/*  5 - Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.*/
+
+// creo una variabile per il div dove stamperò la somma dei numeri
+const stamp = document.querySelector(".stamp");
+// creo un arrey vuoto
+let listNum = [];
+// chiedo all' di inserirer un numero per 6 volte tramite un promt => For {promt}
+for ( let i = 0; i < 6; i++) {
+    const num = parseInt(prompt("inserisci un numero." + ` ${i + 1} di 6`))
+    // se il valore inserito è non è multiplo di due (è pari) lo inserisco nella lista 
+    if (num % 2){
+        listNum.push(num)
+    }
+}
+    console.log(listNum);
